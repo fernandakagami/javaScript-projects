c43eb0d54603519b88770bfe86936f391b6ceef2
Fernanda <fernandakagami@gmail.com>
2022-10-12 15:57:08 -0300

    update project

--- robotron-2000/js/main.js
+++ robotron-2000/js/main.js
@@ -1,6 +1,5 @@
-const controle = document.querySelectorAll(".controle-ajuste");
-const estatisticas = document.querySelectorAll("[data-estatistica]");
-console.log(estatisticas);
+const controle = document.querySelectorAll("[data-controle]")
+const estatistica = document.querySelectorAll("[data-estatistica]")
 
 const pecas = {
     "bracos": {
@@ -36,26 +35,25 @@ const pecas = {
     }
 }
 
-controle.forEach((elemento) => {
-    elemento.addEventListener("click", (evento) => {
-        manipulaDados(evento.target.dataset.controle, evento.target.parentNode);
-        atualizaEstatisticas(evento.target.dataset.peca)
+controle.forEach( (elemento) => {
+    elemento.addEventListener('click', (evento) => {
+        manipulaDados(evento.target.textContent, evento.target.parentNode)
+        atualizaEstatistica(evento.target.dataset.peca)
     })
 })
 
-function manipulaDados(operacao, controle) { 
-    const peca = controle.querySelector("[data-contador]");  
+function manipulaDados(operacao, controle) {
+    const peca = controle.querySelector("[data-contador]")
 
-    if (operacao === "-") {
+    if(operacao === "-") {
         peca.value = parseInt(peca.value) - 1;
     } else {
         peca.value = parseInt(peca.value) + 1;
     }
 }
 
-function atualizaEstatisticas(peca) {
-
-    estatisticas.forEach((elemento) => {
+function atualizaEstatistica(peca) {
+    estatistica.forEach( (elemento ) => {
         elemento.textContent = parseInt(elemento.textContent) + pecas[peca][elemento.dataset.estatistica]
-    });
+    })
 }
