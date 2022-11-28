75dc6cc24c060320167fa3c583a61e39aadf02ff
Fernanda <fernandakagami@gmail.com>
2022-11-28 10:27:09 -0300

    Update index.js

--- bytebank/index.js
+++ bytebank/index.js
@@ -1,11 +1,43 @@
-const cliente1Nome = "Ricardo";
-const cliente1CPF = 111222333309;
-const cliente1Agencia = 1001;
-const cliente1Saldo = 0;
+class Cliente {
+    nome;
+    cpf;    
+}
 
-const cliente2Nome = "Alice";
-const cliente2CPF = 888222333309;
-const cliente2Agencia = 1001;
-const cliente2Saldo = 0;
+class ContaCorrente {
+    agencia;
+    _saldo;
 
-console.log(cliente1Nome);
+    sacar(valor) {
+        if (this._saldo >= valor) {
+            this._saldo -= valor;
+        }
+    }
+
+    depositar(valor) {
+        if (valor > 0) {
+            this._saldo += valor;
+        }
+    }
+}
+
+const cliente1 = new Cliente();
+
+cliente1.nome = "Ricardo";
+cliente1.cpf = 111222333309;
+
+const cliente2 = new Cliente();
+cliente2.nome = "Alice";
+cliente2.cpf = 888222333309;
+
+const contaCorrenteRicardo = new ContaCorrente();
+contaCorrenteRicardo.agencia = 1001;
+
+contaCorrenteRicardo.depositar(100);
+contaCorrenteRicardo.depositar(100);
+contaCorrenteRicardo.depositar(100);
+
+const valorSacado = contaCorrenteRicardo.sacar(50);
+console.log(valorSacado);
+
+console.log(contaCorrenteRicardo);
+
