0e4549f616fea5498932fbb058252210ef70dc48
Fernanda <26352882+fernandakagami@users.noreply.github.com>
2022-12-11 19:58:20 -0300

    Update script.js

--- api-fetch/script.js
+++ api-fetch/script.js
@@ -1,38 +1,75 @@
-let employees = []
-let roles = []
-const listEl = document.querySelector('ul')
-const formEl = document.querySelector('form')
+let employees = [];
+let roles = [];
+let selectedItem;
+const listEl = document.querySelector('ul');
+const formEl = document.querySelector('form');
+const bdelete = document.getElementById("bdelete");
+const bcancel = document.getElementById("bcancel");
+const bsubmit = document.getElementById("bsubmit");
 
 
 async function init() {
   try {
-    [employees, roles] = await Promise.all([listEmployees(), listRoles()])
-    renderData()
-  } catch (erro) {
-    showError(erro)
+    [employees, roles] = await Promise.all([listEmployees(), listRoles()]);
+    renderRoles();
+    renderData();
+    clearSelection();
+    bcancel.addEventListener("click", clearSelection);
+  } catch (erro) {    
+    showError(erro);
   }      
 }
 init();
-  
-function renderData() {  
-  let items = employees.map((employee) => {
-      let role = roles.find((role) => role.id == employee.role_id)
-      return `<li>${employee.name}<br>${role.name}</li>`
-  })
-  listEl.innerHTML = items.join('')
 
-  for (const employees of employees) {
+function selectItem(employee, li) {
+  clearSelection();
+  selectedItem = employee;
+  li.classList.add("selected");
+  formEl.name.value = employee.name;
+  formEl.salary.valueAsNumber = employee.salary;
+  formEl.role_id.value = employee.role_id;
+  bdelete.style.display = 'inline'
+  bcancel.style.display = 'inline'
+}
+
+function clearSelection() {
+  selectedItem = undefined
+  const li = listEl.querySelector(".selected")
+  if (li) {
+    li.classList.remove("selected")
+  }
+  formEl.name.value = '';
+  formEl.salary.value = '';
+  formEl.role_id.value = '';
+  bdelete.style.display = 'none'
+  bcancel.style.display = 'none'
+}
+
+function renderData() {    
+  for (const employee of employees) {
     let role = roles.find((role) => role.id == employee.role_id)
     const li = document.createElement("li")
     const divName = document.createElement("div")
     divName.textContent = employee.name
     const divRole = document.createElement("div")
     divRole.textContent = role.name
+    li.appendChild(divName)
+    li.appendChild(divRole)
     listEl.appendChild(li)
+    li.addEventListener("click", () => selectItem(employee, li))
+  }
+}
+
+function renderRoles() {    
+  for (const role of roles) {    
+    const option = document.createElement("option")    
+    option.textContent = role.name
+    option.value = role.id
+    formEl.role_id.appendChild(option);
   }
 }
 
 function showError(error) {
-  document.getElementById("app").innerHTML = "Error ao carregar dados.";
+  document.getElementById("erros").textContent = "Error ao carregar dados.";
   console.error(error)
 }
